32050d6d2bcabced57d839dac3d791e3cd89fcda
Ahmed Abbasi <alcompilor@gmail.com>
2024-01-18 16:27:43 +0100

    Add API module to fetch comments for a post using the postID parameter

---
+++ js/api/comments.js
@@ -0,0 +1,14 @@
+const getComments = async (postID) => {
+    try {
+        const res = await fetch(
+            `https://dummyjson.com/comments/post/${postID}`
+        );
+        const data = await res.json();
+
+        return data;
+    } catch (err) {
+        throw new Error(`Failed to fetch comments for the post`);
+    }
+};
+
+export { getComments };
