ed16e10e2acea6ebd50c5e78932ba8f0f777cfd7
Ahmed Abbasi <alcompilor@gmail.com>
2024-01-18 16:25:03 +0100

    Add API module to fetch posts - Num of posts can be limited using the "limit" parameter - Only a range of posts will be returned when using "skipID" parameter

---
+++ js/api/posts.js
@@ -0,0 +1,14 @@
+const getPosts = async (limit, skipID = 0) => {
+    try {
+        const res = await fetch(
+            `https://dummyjson.com/posts?limit=${limit}&skip=${skipID}`
+        );
+        const data = await res.json();
+
+        return data;
+    } catch (err) {
+        throw new Error("Failed to fetch posts");
+    }
+};
+
+export { getPosts };
